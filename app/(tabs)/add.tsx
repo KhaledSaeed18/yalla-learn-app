e66ea5abb75629be94ac330e7b419cad617f11e3
KhaledSaeed18 <khaled18saeed@gmail.com>
2025-04-12 17:04:15 +0300

    feat: implement Card, Heading, Image, and Text components with styles and variants

--- app/(tabs)/add.tsx
+++ app/(tabs)/add.tsx
@@ -1,12 +1,54 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, TouchableOpacity } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { Card } from "@/components/ui/card"
+import { Heading } from "@/components/ui/heading"
+import { Text } from "@/components/ui/text"
+import { Image } from "@/components/ui/image"
 
 const add = () => {
+    const handleCard1Click = () => {
+        console.log("Card 1 was clicked - Quick Start")
+    }
+
+    const handleCard2Click = () => {
+        console.log("Card 2 was clicked - Quick Start")
+    }
+
     return (
         <SafeAreaView className='bg-background-0 flex-1'>
-            <View className=''>
-                <Text className='text-xl font-bold mb-2 text-typography-700'>Add</Text>
+            <View className='px-3 flex flex-col gap-3'>
+                <TouchableOpacity activeOpacity={0.7} onPress={handleCard1Click}>
+                    <Card size="lg" variant="outline">
+                        <Image
+                            source={{
+                                uri: "https://gluestack.github.io/public-blog-video-assets/yoga.png",
+                            }}
+                            className="mb-2 h-[240px] w-full rounded-md"
+                            alt="image"
+                        />
+                        <Heading size="md" className="mb-1">
+                            Quick Start
+                        </Heading>
+                        <Text size="sm">Start building your next project in minutes</Text>
+                    </Card>
+                </TouchableOpacity>
+
+                <TouchableOpacity activeOpacity={0.7} onPress={handleCard2Click}>
+                    <Card size="lg" variant="outline">
+                        <Image
+                            source={{
+                                uri: "https://gluestack.github.io/public-blog-video-assets/yoga.png",
+                            }}
+                            className="mb-2 h-[240px] w-full rounded-md"
+                            alt="image"
+                        />
+                        <Heading size="md" className="mb-1">
+                            Quick Start
+                        </Heading>
+                        <Text size="sm">Start building your next project in minutes</Text>
+                    </Card>
+                </TouchableOpacity>
             </View>
         </SafeAreaView>
     )
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,11 @@
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { Text } from "@/components/ui/text"
 
 export default function Tab() {
     return (
-        <SafeAreaView className='bg-red-400 flex-1'>
-            <View className='bg-blue-400'>
+        <SafeAreaView className='bg-background-0 flex-1'>
+            <View className=''>
                 <Text className='text-xl font-bold mb-2 text-typography-700'>Home</Text>
             </View>
         </SafeAreaView>
