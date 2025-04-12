caeaa19e10b97d30c87fae94ab675a4834f2fed6
KhaledSaeed18 <khaled18saeed@gmail.com>
2025-04-12 15:53:37 +0300

    feat: add Button and Toast components with styles and dependencies

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,13 @@
 import { Box } from "@/components/ui/box";
+import { Button, ButtonText } from "@/components/ui/button";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View className="flex items-center justify-center h-screen bg-white">
-      <Box
-        className="bg-primary-500 p-5"
-      >
-        <Text className='text-typography-0'>
-          This is the Box
-        </Text>
-      </Box>
+      <Button size="md" variant="solid" action="primary">
+        <ButtonText>Hello World!</ButtonText>
+      </Button>
     </View>
   );
 }
