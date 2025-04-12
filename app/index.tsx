5ffcd4df35fdd5ce3fe2b9760d01a294739e28f0
KhaledSaeed18 <khaled18saeed@gmail.com>
2025-04-12 15:19:08 +0300

    feat: implement Box component with styles for both web and native platforms

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,16 @@
+import { Box } from "@/components/ui/box";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex items-center justify-center h-screen bg-white">
+      <Box
+        className="bg-primary-500 p-5"
+      >
+        <Text className='text-typography-0'>
+          This is the Box
+        </Text>
+      </Box>
     </View>
   );
 }
